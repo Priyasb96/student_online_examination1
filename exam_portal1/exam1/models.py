c13bb32ffcd8720533f6509dff3d147f573fe944
Priyasb96 <priyasb21@gmail.com>
2025-07-09 11:59:24 +0530

    create project

--- exam_portal1/exam1/models.py
+++ exam_portal1/exam1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class classtype(models.Model):
+    questions=models.IntegerField(unique=True),
+    type_questions=models.IntegerField(unique=True)
