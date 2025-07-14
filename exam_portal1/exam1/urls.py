cd2ce62d99aba1d567b54c83a8194c0ffc87f82a
Priyasb96 <priyasb21@gmail.com>
2025-07-14 15:32:47 +0530

    created project

--- exam_portal1/exam1/urls.py
+++ exam_portal1/exam1/urls.py
@@ -18,5 +18,9 @@ from exam1 import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('otp/', views.verify_otp, name='verify_otp'),
+
+
 ]
--- exam_portal1/exam1/views.py
+++ exam_portal1/exam1/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-# def home(request):
-#     return render(request, 'exam1\login.html')
 
-def home(request):
-     return render(request, 'exam1/register.html')
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
-# def home(request):
-#     return render(request, 'exam1/dashboard.html')
+def register_view(request):
+     form = RegisterForm()
+     if request.method == 'POST':
+          form = RegisterForm(request.POST)
+          if form.is_valid():
+               form.save()
+               messages.success(request, "Register Successful")
+               return redirect('login')
+     return render(request, 'exam1/register.html',{'form': form})
 
+def login_view(request):
+     if request.method == "POST":
+          username = request.POST.get("username")
+          password = request.POST.get("password")
+
+          user = authenticate(request, username=username, password=password)
+          if user:
+               login(request,user)
+               return redirect('admin')
+          else:
+               messages.error(request, "Invalid username or password.")
+               return redirect('login')
+     return render(request, 'exam1/login.html')
+
+def verify_otp(request):
+      messages.info(request, "OTP verification placeholder.")
+      return redirect('admin')
+
+# def logout_view(request):
+#       logout
+#       return render(request, 'exam1/dashboard.html')
